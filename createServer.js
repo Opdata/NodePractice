c567bde2f68adbf8b5552e4a8ce710c0335ad81f
Opdata <pb03641@gmail.com>
2019-12-02 20:37:57 +0900

    Edit createServer Test source

--- createServer.js
+++ createServer.js
@@ -3,6 +3,9 @@ const http = require("http");
 http.createServer(
   (req,
   res => {
-    // Edit
+    res.write("<h1> Hello Node </h1>");
+    res.end("<p> Hello Server! </p>");
+  }).listen(8080, () => {
+    console.log("8080포트에서 서버 대기 중");
   })
 );
